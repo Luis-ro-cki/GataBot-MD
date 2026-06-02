48e555849313d117f0a524eb91e42a1182cc204c
Luis-ro-cki <l29472954@gmail.com>
2026-06-01 18:26:39 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ import { ar, en, es, id, pt } from './lib/idiomas/total-idiomas.js'
 // [ES] > Agrega el número que será Bot y los que serán propietarios.
 // [EN] > Add the number that will be Bot and those that will be owners.
 global.owner = [
-'5214531287294', 
+'50578391933', 
 '593968263524',
 '5492916450307',
 '51994729892',
